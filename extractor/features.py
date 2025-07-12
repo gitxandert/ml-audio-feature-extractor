8bfd79912f5a706d2ce5897f94ef766a770bd2ae
gitxandert <alexandertoth96@yahoo.com>
2025-07-12 11:30:13 -0400

    restyle for virtual environment

--- extractor/features.py
+++ extractor/features.py
@@ -2,9 +2,13 @@ import torchaudio.pipelines
 import torch
 import numpy as np
 import umap
-import matplotlib.pyplot as plt
+import os, matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, FFMpegFileWriter
+
+import matplotlib
+matplotlib.use("Agg")
 
 bundle = torchaudio.pipelines.WAV2VEC2_BASE
 
@@ -32,49 +36,14 @@ ax = fig.add_subplot(projection="3d")
 x = embeddings_3d[:, 0]
 y = embeddings_3d[:, 1]
 z = embeddings_3d[:, 2]
-colors = plt.cm.plasma(np.linspace(0,1,len(x)))
+colors = cm.plasma(np.linspace(0,1,len(x)))
 
-scatter = ax.scatter([x[0]], [y[0]], [z[0]], c=[colors[0]], s=5)
+scatter = ax.scatter(x, y, z, c=colors, s=5)
 
-import matplotlib.cm as cm
-import matplotlib.colors as mcolors
-norm = mcolors.Normalize(vmin=0, vmax=len(x))
-sm = plt.cm.ScalarMappable(cmap=plt.cm.plasma, norm=norm)
-sm.set_array([])
-fig.colorbar(sm, ax=ax, label="TimeStep")
+ax.set(title="W2V2 + UMAP 3D", xlabel="UMAP1", ylabel="UMAP2", zlabel="UMAP3")
+cb = plt.colorbar(scatter, ax=ax, shrink=0.6, pad=0.1)
+cb.set_label("Frame index")
 
-ax.set_title("Wav2Vec2 Embeddings (UMAP 3D Projection)")
-ax.set_xlabel("UMAP Dim 1")
-ax.set_ylabel("UMAP Dim 2")
-ax.set_zlabel("UMAP Dim 3")
-
-ax.set_xlim(x.min(), x.max())
-ax.set_ylim(y.min(), y.max())
-ax.set_zlim(z.min(), z.max())
-
-def update(i):
-    ax.cla()
-    ax.set_xlim(x.min(), x.max())
-    ax.set_ylim(y.min(), y.max())
-    ax.set_zlim(z.min(), z.max())
-    ax.set_title("Wav2Vec2 Embeddings (UMAP 3D Projection)")
-    ax.set_xlabel("UMAP Dim 1")
-    ax.set_ylabel("UMAP Dim 2")
-    ax.set_zlabel("UMAP Dim 3")
-    ax.scatter(x[:i], y[:i], z[:i], c=colors[:i], s=5)
-    return ax,
-
-ani = FuncAnimation(
-    fig,
-    update,
-    frames=len(x),
-    interval=30,
-    blit=False,
-    repeat=False
-)
-
-import os
-out_path = "embedding_animation.gif"
-ani.save(out_path, writer="pillow", fps=30)
-print("Saved animation to:", os.path.abspath(out_path))
-# plt.show()
+out_path = "umap_scatter.png"
+plt.savefig(out_path, dpi=150, bbox_inches="tight")
+print(f"Saved scatter to {out_path}")
