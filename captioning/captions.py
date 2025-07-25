e4b23add0ac650280e92f7e4494833ba2d415f89
gitxandert <alexandertoth96@yahoo.com>
2025-07-25 13:50:25 -0400

    return model(path)['cands'][0]

--- captioning/captions.py
+++ captioning/captions.py
@@ -7,12 +7,7 @@ def load_conette_model():
     return model
 
 def caption_audio(path, model):
+    print(f"    Generating captions...")
     outputs = model(path)
 
-    return outputs
-
-path = "tests/audio/424832__sctang__funny-pilot-captain-speaking-after-landing.wav"
-model = load_conette_model()
-captions = caption_audio(path, model)
-
-print(captions)
+    return outputs['cands'][0]
