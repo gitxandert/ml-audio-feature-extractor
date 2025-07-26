aa11301a33336a3b25a31497a1e7b748f17fc3b8
gitxandert <alexandertoth96@yahoo.com>
2025-07-26 00:54:28 -0400

    move clustering functions to new file and add cluster name to metadata

--- extractor/features.py
+++ extractor/features.py
@@ -1,12 +1,8 @@
 import torchaudio.pipelines
 import torch
-import numpy as np
-import umap
 import os, shutil, subprocess, tempfile, pickle
-import matplotlib.pyplot as plt
 from projections.projection_head import ProjectionHead
-from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import normalize
+from extractor.clustering import cluster_results
 from indexer.faiss_indexer import build_faiss_index, load_faiss_index, save_faiss_index, search_faiss_index
 from indexer.metadata import insert_metadata_rows, get_metadata_by_id
 from captioning.captions import load_conette_model, caption_audio
@@ -199,108 +195,13 @@ def create_metadata_rows(results):
                 'start_time': r['start_times'][e],
                 'duration': r['file_duration'],
                 'domain': r['domain'],
+                'cluster': r['cluster'],
                 'caption': r['caption']
-                # 'cluster'
             }
             rows.append(row)
             id += 1
     
     return rows
-
-def reduce_to_nd(embeddings: torch.Tensor, dim: int=10) -> np.ndarray:
-    embeddings_numpy = embeddings.numpy()
-    n_samples = embeddings_numpy.shape[0]
-
-    dim = max(1, min(dim, n_samples - 2))
-    n_neighbors = max(2, min(15, n_samples - 1))
-    
-    reducer = umap.UMAP(n_components=dim, n_neighbors=n_neighbors)
-    embeddings_nd = reducer.fit_transform(embeddings_numpy)
-    return embeddings_nd
-
-def plot_clusters(data, labels, centers=None, title="GMM Clustering", save_path="cluster_plot.png"):
-    print("Plotting clusters...")
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(projection='3d')
-
-    colors = []
-    for label in np.unique(labels):
-        cluster_points = data[labels == label]
-        x = cluster_points[:, 0]
-        y = cluster_points[:, 1]
-        z = cluster_points[:, 2]
-        scatter = ax.scatter(x, y, z, label=f'Cluster {label}', s=40, alpha=0.7)
-        new_color = scatter.get_facecolor()[0]
-        colors.append(new_color)
-    
-    if centers is not None:
-        ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c=colors, marker='x', s=100, label='Centers')
-    
-    ax.legend()
-    ax.set_title(title)
-    ax.set_xlabel("Dim 1")
-    ax.set_ylabel("Dim 2")
-    ax.set_zlabel("Dim 3")
-    plt.tight_layout()
-    plt.savefig(save_path)
-    print(f"Saved plot to: {save_path}")
-
-def fit_gmms_with_hybrid_score(data, max_components=10, force=False, force_k=10):
-    print("Fitting GMMs...")
-
-    from sklearn.metrics import silhouette_score
-
-    best_score = -1
-    best_k = 0
-    best_model = None
-
-    scores = []
-
-    if force:
-        best_k = force_k
-        best_model = GaussianMixture(n_components=best_k).fit(data)
-    else:
-        for k in range(2, max_components + 1):
-            gmm = GaussianMixture(n_components=k).fit(data)
-            labels = gmm.predict(data)
-            score = silhouette_score(data, labels)
-            if score > best_score:
-                best_score = score
-                best_k = k
-                best_model = gmm
-            scores.append((k, score))
-        
-    labels = best_model.predict(data)
-
-    reducer = umap.UMAP(n_components=3, random_state=144)
-    plottable_data = reducer.fit_transform(data)
-    plottable_centers = reducer.transform(best_model.means_)
-    plot_clusters(plottable_data, labels, centers=plottable_centers)
-
-    return best_model, best_k, labels
-
-# def cluster_files(filepaths, data, model, top_dir="clusters"):
-#     print(f"Clustering files into {top_dir}")
-#     if os.path.exists(top_dir) and os.path.isdir(top_dir):
-#         shutil.rmtree(top_dir)
-
-#     labels = model.predict(data)
-
-#     sorted_indices = np.argsort(labels)
-
-#     sorted_labels = labels[sorted_indices]
-#     sorted_paths = [filepaths[i] for i in sorted_indices]
-
-#     current_cluster = None
-#     cluster_dir = None
-#     for label, path in zip(sorted_labels, sorted_paths):
-#         if label != current_cluster:
-#             current_cluster = label
-#             cluster_dir = os.path.join(top_dir, f"{label}")
-#             os.makedirs(cluster_dir, exist_ok=True)
-#             print(f"\nCluster {label}:")
-#         shutil.move(path, cluster_dir)
-#         print(f"  {os.path.basename(path)}")
 
 def open_pkl(pkl_path):
     with open(pkl_path, "rb") as f:
@@ -315,11 +216,14 @@ print("Loading metadata...")
 # results = process_audio_files("tests/audio")
 results = open_pkl("pkls/captioned_results.pkl")
 
-embeddings = torch.cat([r['embeddings'] for r in results], dim=0).detach().cpu().numpy()
+clustered_results = cluster_results(results)
+flat_results = [cr for cluster in clustered_results for cr in cluster]
+
+embeddings = torch.cat([fr['embeddings'] for fr in flat_results], dim=0).detach().cpu().numpy()
 create_faiss_index(embeddings)
 
 db = "data/metadata.sqlite"
-rows = create_metadata_rows(results)
+rows = create_metadata_rows(flat_results)
 insert_metadata_rows(db, rows)
 
 print("\nID numbers correspond to chunks of audio.")
@@ -354,4 +258,5 @@ while True:
         print(f"    Duration: {file_duration}")
         print(f"    Chunk start time: {start}")
         print(f"    Domain: {domain}")
+        print(f"    Cluster: {cluster}")
         print(f"    Caption: {caption}")
